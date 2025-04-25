f249c1ea727d4f6a7309f5949985e91829e35e62
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2025-04-26 01:57:51 +0500

    bubble sorting algo added

--- advance_algorithms/Algorithms.java
+++ advance_algorithms/Algorithms.java
@@ -3,8 +3,7 @@ package advance_algorithms;
 import java.util.Arrays;
 
 public class Algorithms {
-    public static void inPlaceSortingAlgo() {
-        int[] num_arr = { 54, 78, 98, 43, 22, 41, 7, 9, 4, 25, 59 };
+    public static void inPlaceSortingAlgo(int[] num_arr) {
         for (int i = 0; i < num_arr.length; i++) {
             int smallestNum = num_arr[i];
             for (int j = i; j < num_arr.length; j++) {
@@ -19,8 +18,31 @@ public class Algorithms {
         System.out.println("Sorted Array:\t" + Arrays.toString(num_arr));
     }
 
+    // bubble sorting algorithm
+    public static void bubbleSortingAlgo(int[] num_arr) {
+        for (int i = 0; i < num_arr.length; i++) {
+            for (int j = 0; j < num_arr.length - 1; j++) {
+                int left_element = num_arr[j];
+                int right_element = num_arr[j + 1];
+                if (left_element > right_element) {
+                    int temp_num = left_element;
+                    num_arr[j] = right_element;
+                    num_arr[j + 1] = left_element;
+                    left_element = right_element;
+                    right_element = temp_num;
+                }
+            }
+        }
+        System.out.println("Sorted Array:\t" + Arrays.toString(num_arr));
+    }
+
     public static void main(String args[]) {
-        // in-place sorting OR Selection sorting
-        inPlaceSortingAlgo();
+        int[] num_arr = { 54, 78, 98, 43, 22, 41, 7, 9, 4, 25, 59 };
+
+        // // in-place sorting OR Selection sorting
+        // inPlaceSortingAlgo(num_arr);
+
+        // bubble sorting
+        bubbleSortingAlgo(num_arr);
     }
 }
